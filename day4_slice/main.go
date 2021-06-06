eeb6e0131c3aef1fe207948610b40775d953c6e3
shi.hao <shi.hao@puscene.com>
2021-06-06 10:41:36 +0800

    slice append split

--- day4_slice/main.go
+++ day4_slice/main.go
@@ -24,6 +24,37 @@ func testSlice() {
 	}
 }
 
+func sliceAppend() {
+	var numbers = make([]int, 3, 5)
+	fmt.Println(len(numbers), cap(numbers), numbers)
+	numbers = append(numbers, 1)
+	fmt.Println(len(numbers), cap(numbers), numbers)
+	numbers = append(numbers, 1)
+	fmt.Println(len(numbers), cap(numbers), numbers)
+	numbers = append(numbers, 1)
+	fmt.Println(len(numbers), cap(numbers), numbers)
+
+	var numbers2 = make([]int, 3)
+	numbers2 = append(numbers2, 1)
+	fmt.Println(len(numbers2), cap(numbers2), numbers2)
+}
+
+func sliceSplit() {
+	s1 := []int{1, 2, 3}
+	s2 := s1[0:2]
+	fmt.Println(len(s2), cap(s2), s2)
+	s2[0] = 100
+	fmt.Println(s1, s2)
+
+	s3 := make([]int, 4)
+	copy(s3, s1)
+	fmt.Println(len(s3), cap(s3), s3)
+	s3[0] = 10000
+	fmt.Println(s1, s3)
+}
+
 func main() {
-	testSlice()
+	// testSlice()
+	// sliceAppend()
+	sliceSplit()
 }
