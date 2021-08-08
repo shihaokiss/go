47a362e488ce3ec5e1a76d7e50e19a6c05e2b51e
shi.hao <shi.hao@puscene.com>
2021-08-08 09:42:48 +0800

    day9

---
+++ day9_IM_SYSTEM/server.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"sync"
+	"time"
+)
+
+type Server struct {
+	Ip         string
+	Port       int
+	mapLock    sync.RWMutex
+	OnlineUser map[string]*User
+	Message    chan string
+}
+
+//创建一个服务对象
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:         ip,
+		Port:       port,
+		OnlineUser: make(map[string]*User),
+		Message:    make(chan string),
+	}
+	return server
+}
+
+//启动服务器的接口, Server 类的一个方法
+func (this *Server) Start() {
+	//listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+
+	// 函数推出时候关闭 close
+	defer listener.Close()
+	go this.ListenMessageChannel()
+
+	for {
+		//accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener.accept err:", err)
+			continue
+		}
+
+		//dohandle
+		go this.DoHandle(conn)
+	}
+}
+
+// 向message管道中写信息,并广播
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + "-" + user.Name + "-" + msg
+	this.Message <- sendMsg
+}
+
+// 监听message 管道的goroutine, 把消息分发到各个用户
+func (this *Server) ListenMessageChannel() {
+	for {
+		msg := <-this.Message
+		this.mapLock.Lock()
+		for _, user := range this.OnlineUser {
+			user.Channel <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+// Server dohandle 的方法
+func (this *Server) DoHandle(conn net.Conn) {
+	fmt.Println("do handle start...")
+	user := NewUser(conn, this)
+	user.Online()
+
+	//监听用户是否活跃的channel
+	isLive := make(chan bool)
+
+	//接受客户端的消息并进行广播
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				//用户下线
+				user.Offline()
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println(user.Addr, " read error:", err)
+				return
+			}
+			//提取用户的消息,去除 '\n'
+			msg := string(buf[:n-1])
+
+			//用户处理消息
+			user.DoMessage(msg)
+			isLive <- true
+		}
+	}()
+
+	//handle 阻塞
+	for {
+		select {
+		case <-isLive:
+			//用户活跃，检测time.After的时候会归0触发器
+		case <-time.After(time.Second * 10):
+			//超时了，需要踢出用户
+			user.SendMsg("timeout!")
+			user.Clean()
+		}
+	}
+}
---
+++ day9_IM_SYSTEM/user.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+
+type User struct {
+	Name    string
+	Addr    string
+	Channel chan string
+	Conn    net.Conn
+	Server  *Server
+}
+
+//创建一个用户信息
+func NewUser(conn net.Conn, server *Server) *User {
+	addr := conn.RemoteAddr().String()
+	user := &User {
+        Name: addr,
+		Addr: addr,
+		Channel: make(chan string),
+		Conn: conn,
+		Server: server,
+	}
+
+	//启动监听管道中的goroutine(go程)
+	fmt.Println("NewUser ListenMessage start.")
+	go user.ListenMessage()
+	return user
+}
+
+//客户端监听管道
+func (this *User) ListenMessage() {
+	for {
+		//监听到管道中有数据，就把信息写到对端
+		message := <-this.Channel
+		this.SendMsg(message)
+	}
+}
+
+//用户上线
+func (this *User) Online() {
+	//用户上线
+	this.Server.mapLock.Lock()
+	this.Server.OnlineUser[this.Name] = this
+	this.Server.mapLock.Unlock()
+    this.Server.BroadCast(this, "online!")
+}
+
+//用户下线
+func (this *User) Offline() {
+	//用户下线
+	this.Server.mapLock.Lock()
+	delete(this.Server.OnlineUser, this.Name)
+	this.Server.mapLock.Unlock()
+    this.Server.BroadCast(this, "offline!")
+}
+
+//清理用户资源
+func (this *User) Clean() {
+	close(this.Channel)
+	this.Conn.Close()
+}
+
+//发送消息
+func (this *User) SendMsg(msg string) {
+	this.Conn.Write([]byte(msg+"\n"))
+}
+
+//用户消息处理
+func (this *User) DoMessage(msg string) {
+	//服务端进行广播
+	if msg == "who" {
+        this.Server.mapLock.Lock()
+		for _, user := range this.Server.OnlineUser {
+			msg := "[" + user.Addr + "]" + user.Name + ": online!"
+			this.SendMsg(msg)
+		}
+		this.Server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		newName := strings.Split(msg, "|")[1]
+        // 判断有没有用户重名
+		_, ok := this.Server.OnlineUser[newName]
+		if ok {
+			this.SendMsg(newName + " has already exist!")
+		} else {
+			this.Server.mapLock.Lock()
+			delete(this.Server.OnlineUser, this.Name)
+			this.Server.OnlineUser[newName] = this
+			this.Server.mapLock.Unlock()
+
+			this.Name = newName
+			this.SendMsg("rename success! user=" + newName)
+		}
+
+	} else {
+		this.Server.BroadCast(this, msg)
+	}
+}
